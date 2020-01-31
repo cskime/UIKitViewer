c9a10bf604913fc69598a4a7a2969741d88b4a4e
cskim <kcsol1005@gmail.com>
2020-01-31 20:01:40 +0900

    Initial Project

---
+++ UIKitViewer/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  UIKitViewer
+//
+//  Created by cskim on 2020/01/31.
+//  Copyright © 2020 cskim. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+  var window: UIWindow?
+
+  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    
+    window = UIWindow(frame: UIScreen.main.bounds)
+    let vc = ViewController()
+    window?.rootViewController = UINavigationController(rootViewController: vc)
+    window?.makeKeyAndVisible()
+    
+    return true
+  }
+}
+
