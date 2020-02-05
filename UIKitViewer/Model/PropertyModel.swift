1fea5f75ce2d141cbb4f49b64a6cc725d8cd068d
sj2joy <sj2joy@gmail.com>
2020-02-05 21:19:04 +0900

    PropertyModel

--- UIKitViewer/Model/PropertyModel.swift
+++ UIKitViewer/Model/PropertyModel.swift
@@ -19,64 +19,90 @@ struct Property {
   var name: String
   var controlType: ControlType
 }
-
 var properties: [String: [Property]] = [
   "UIButton" : [
     Property(name: "setTitle", controlType: .textField),
-    Property(name: "setImage", controlType: .toggle),
     Property(name: "setTitleColor", controlType: .palette),
+    Property(name: "image", controlType: .toggle),
+    Property(name: "setImage", controlType: .toggle),
+    Property(name: "setBackgroundImage", controlType: .toggle)
   ],
   "UILabel" : [
     Property(name: "text", controlType: .textField),
+    Property(name: "font", controlType: .textField),
     Property(name: "textColor", controlType: .palette),
+    Property(name: "minimumScaleFactor", controlType: .slider),
+    Property(name: "numberOfLines", controlType: .slider)
   ],
   "UIView" : [
     Property(name: "contentMode", controlType: .toggle),
     Property(name: "tintColor", controlType: .palette),
     Property(name: "backgroundColor", controlType: .palette),
-  ],
+    Property(name: "clipsToBounds", controlType: .toggle),
+    Property(name: "alpha", controlType: .slider),
+    Property(name: "isHidden", controlType: .toggle)
+    ],
   "UISwitch" : [
     Property(name: "isOn", controlType: .toggle),
-    Property(name: "onImage", controlType: .toggle),
-    Property(name: "offImage", controlType: .toggle),
+    Property(name: "setOn", controlType: .toggle),
+    Property(name: "onTintColor", controlType: .palette),
+    Property(name: "thumbTintColor", controlType: .palette),
   ],
   "UIStepper" : [
     Property(name: "wraps", controlType: .toggle),
     Property(name: "stepValue", controlType: .slider),
-    Property(name: "autoRepeat", controlType: .toggle),
-    Property(name: "isContinuous", controlType: .toggle),
+    Property(name: "decrementImage", controlType: .toggle),
+    Property(name: "setDecrementImage", controlType: .toggle),
+    Property(name: "dividerImage", controlType: .toggle),
+    Property(name: "setDividerImage", controlType: .toggle)
   ],
   "UITextField" : [
     Property(name: "font", controlType: .slider),
+    Property(name: "attributedText", controlType: .textField),
     Property(name: "textColor", controlType: .palette),
-    Property(name: "placeholder", controlType: .slider),
+    Property(name: "placeholder", controlType: .textField),
     Property(name: "borderStyle", controlType: .slider),
-    Property(name: "textAlignment", controlType: .toggle),
+    Property(name: "clearButtonMode", controlType: .toggle),
+    Property(name: "disabledBackground", controlType: .toggle),
   ],
   "UITableView" : [
     Property(name: "style", controlType: .slider),
     Property(name: "rowHeight", controlType: .slider),
     Property(name: "numberOfRows", controlType: .slider),
+    Property(name: "backgroundView", controlType: .toggle),
+    Property(name: "separatorStyle", controlType: .toggle),
+    Property(name: "separatorColor", controlType: .palette),
     Property(name: "numberOfSections", controlType: .slider),
-    Property(name: "dequeueReusableCell", controlType: .slider),
   ],
   "UICollectionView" : [
     Property(name: "numberOfItems", controlType: .slider),
     Property(name: "numberOfSections", controlType: .slider),
-    Property(name: "collectionViewLayout", controlType: .slider),
-    Property(name: "layoutAttributesForSupplementaryElemnet", controlType: .slider),
-  ],
+    Property(name: "insertItems", controlType: .toggle),
+    Property(name: "moveItem", controlType: .toggle),
+    Property(name: "deleteItem", controlType: .toggle),
+    ],
+  "UICollectionViewFlowLayout" : [
+    Property(name: "minimumLineSpacing", controlType: .slider),
+    Property(name: "minimumInteritemSpacing",controlType: .slider),
+    Property(name: "itemSize", controlType: .slider),
+    Property(name: "sectionInset", controlType: .slider),
+    Property(name: "headerReferenceSize", controlType: .slider),
+    Property(name: "footerReferenceSize", controlType: .slider),
+    ],
   "UIImageView" : [
-    Property(name: "isHighlighted", controlType: .slider),
+    Property(name: "isHighlighted", controlType: .toggle),
     Property(name: "highlightImage", controlType: .slider),
-    Property(name: "isUserInteractionEnabled", controlType: .slider),
+    Property(name: "isUserInteractionEnabled", controlType: .toggle),
+    Property(name: "startAnimating", controlType: .toggle),
+    Property(name: "stopAnimating", controlType: .toggle),
   ],
   "UIPageControl" : [
-    Property(name: "currentPage", controlType: .slider),
+    Property(name: "size", controlType: .slider),
     Property(name: "numberOfPages", controlType: .slider),
   ],
   "UISegmentedControl" : [
-    Property(name: "ismomentary", controlType: .slider),
+    Property(name: "setDividerImage", controlType: .toggle),
     Property(name: "numberOfSegment", controlType: .slider),
+    Property(name: "removeAllSegment", controlType: .toggle),
   ]
 ]
