283a1f3ee9f9416f8afcd106aabe4d7de6575509
Chamsol Kim <kcsol1005@gmail.com>
2022-08-21 17:21:55 +0900

    PropertyInfo struct를 별도의 파일로 분리

---
+++ UIKitViewer/Application/Model/PropertyInfo.swift
@@ -0,0 +1,14 @@
+//
+//  PropertyInfo.swift
+//  UIKitViewer
+//
+//  Created by Chamsol Kim on 2022/08/21.
+//  Copyright © 2022 cskim. All rights reserved.
+//
+
+import Foundation
+
+struct PropertyInfo {
+    var name: String
+    var controlType: ControlType
+}
