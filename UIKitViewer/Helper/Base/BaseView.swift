f115ad544f66762a1b9ca558539ed4e413bf34b9
Chamsol Kim <kcsol1005@gmail.com>
2022-08-27 10:36:58 +0900

    Implement: Base class 추가

---
+++ UIKitViewer/Helper/Base/BaseView.swift
@@ -0,0 +1,39 @@
+//
+//  BaseView.swift
+//  UIKitViewer
+//
+//  Created by Chamsol Kim on 2022/08/27.
+//  Copyright © 2022 cskim. All rights reserved.
+//
+
+import UIKit
+
+class BaseView: UIView {
+    
+    // MARK: - Initialize
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setUpAttributes()
+        setUpSubviews()
+        setUpConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Set Up
+    
+    func setUpAttributes() {
+        
+    }
+    
+    func setUpSubviews() {
+        
+    }
+    
+    func setUpConstraints() {
+        
+    }
+}
